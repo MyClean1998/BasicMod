170beff33034dcda169625d26c7d664266e157ea
Zoey Ling <zl64@rice.edu>
2025-06-15 17:04:23 -0700

    add Tail of fury relic

---
+++ src/main/java/ninetailsmod/relics/TailOfFury.java
@@ -0,0 +1,35 @@
+package ninetailsmod.relics;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import ninetailsmod.characters.NineTailsCharacter;
+
+import static ninetailsmod.BasicMod.makeID;
+
+public class TailOfFury extends BaseRelic  {
+
+    private static final String NAME = "TailOfFury"; //The name will be used for determining the image file as well as the ID.
+    public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
+    private static final RelicTier RARITY = RelicTier.SPECIAL; //The relic's rarity. TODO: make sure if doesn't show up elsewhere
+    private static final LandingSound SOUND = LandingSound.MAGICAL; //The sound played when the relic is clicked.
+
+    public TailOfFury() {
+        super(ID, NAME, NineTailsCharacter.Meta.CARD_COLOR, RARITY, SOUND);
+    }
+
+    public void atBattleStart() {
+        flash();
+        addToTop((AbstractGameAction)new ApplyPowerAction((AbstractCreature)AbstractDungeon.player, (AbstractCreature)AbstractDungeon.player, (AbstractPower)new StrengthPower((AbstractCreature)AbstractDungeon.player, 1), 1));
+        addToTop((AbstractGameAction)new RelicAboveCreatureAction((AbstractCreature)AbstractDungeon.player, this));
+    }
+
+    public AbstractRelic makeCopy() {
+        return new TailOfFury();
+    }
+}
