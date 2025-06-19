6fdca719b07c25319b0a0795827a68fbe7f62c7a
MyClean1998 <zl41@rice.edu>
2025-06-18 22:45:03 -0700

    [new card] GodFather

--- src/main/java/ninetailsmod/cards/powers/GodFather.java
+++ src/main/java/ninetailsmod/cards/powers/GodFather.java
@@ -1,39 +1,46 @@
 package ninetailsmod.cards.powers;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import ninetailsmod.cards.BaseCard;
 import ninetailsmod.characters.NineTailsCharacter;
 import ninetailsmod.util.CardStats;
 
-public class UncommonCard3 extends BaseCard {
+public class GodFather extends BaseCard {
 
-    public static final String ID = makeID("UncommonCard3"); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    public static final String ID = makeID(GodFather.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
     private static final CardStats info = new CardStats(
             NineTailsCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            -1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
 
-    //These will be used in the constructor. Technically you can just use the values directly,
-    //but constants at the top of the file are easy to adjust.
-    private static final int STRENGTH = 2;
-    private static final int UPG_STRENGTH = 1;
-
-    public UncommonCard3() {
+    public GodFather() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
-        setMagic(STRENGTH, UPG_STRENGTH); //Sets the card's damage and how much it changes when upgraded.
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
+        int effect = 0;
+        if (this.energyOnUse != -1)
+            effect = this.energyOnUse;
+        if (p.hasRelic("Chemical X")) {
+            effect += 2;
+            p.getRelic("Chemical X").flash();
+        }
+        if (this.upgraded)
+            effect++;
+        if (effect > 0) {
+            addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, effect)));
+            addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, effect)));
+            if (!this.freeToPlayOnce)
+                p.energy.use(EnergyPanel.totalCount);
+        }
     }
 }
