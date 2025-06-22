52fcd9fe26fe10b650ad5fc5f93f91f4711c1064
Zoey Ling <zl64@rice.edu>
2025-06-21 20:48:18 -0700

    add hide

--- src/main/java/ninetailsmod/cards/powers/Grace.java
+++ src/main/java/ninetailsmod/cards/powers/Grace.java
@@ -14,20 +14,14 @@ public class Grace extends BaseCard {
     private static final CardStats info = new CardStats(
             NineTailsCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
-
-    //These will be used in the constructor. Technically you can just use the values directly,
-    //but constants at the top of the file are easy to adjust.
-
-    private static final int STRENGTH = 2;
-    private static final int UPG_STRENGTH = 1;
 
     public Grace() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
-        setMagic(STRENGTH, UPG_STRENGTH); //Sets the card's damage and how much it changes when upgraded.
+        setCostUpgrade(0);
     }
 
     @Override
--- src/main/java/ninetailsmod/cards/skills/Hide.java
+++ src/main/java/ninetailsmod/cards/skills/Hide.java
@@ -1,27 +1,29 @@
 package ninetailsmod.cards.skills;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.BlurPower;
 import ninetailsmod.cards.BaseCard;
 import ninetailsmod.characters.NineTailsCharacter;
 import ninetailsmod.util.CardStats;
 
-public class RareSkill extends BaseCard {
+public class Hide extends BaseCard {
 
-    public static final String ID = makeID(RareSkill.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    public static final String ID = makeID(Hide.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
     private static final CardStats info = new CardStats(
             NineTailsCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
 
-    private static final int BLOCK = 10;
-    private static final int UPG_BLOCK = 5;
+    private static final int BLOCK = 6;
+    private static final int UPG_BLOCK = 3;
 
-    public RareSkill() {
+    public Hide() {
         super(ID, info);
 
         setBlock(BLOCK, UPG_BLOCK); //Sets the card's damage and how much it changes when upgraded.
@@ -30,5 +32,6 @@ public class RareSkill extends BaseCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new GainBlockAction(p, p, block));
+        addToBot(new ApplyPowerAction(p, p, new BlurPower(p, 1), 1));
     }
 }
