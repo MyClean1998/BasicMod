e8f630896283475c92a3a3dddee468ada687f551
Zoey Ling <zl64@rice.edu>
2025-07-03 22:56:00 -0700

    add frenzied claw

---
+++ src/main/java/ninetailsmod/cards/attacks/FrenziedClaw.java
@@ -0,0 +1,83 @@
+package ninetailsmod.cards.attacks;
+
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ClawEffect;
+import ninetailsmod.cards.BaseCard;
+import ninetailsmod.characters.NineTailsCharacter;
+import ninetailsmod.util.CardStats;
+
+public class FrenziedClaw extends BaseCard {
+
+    public static final String ID = makeID(FrenziedClaw.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    private static final CardStats info = new CardStats(
+            NineTailsCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    private static final int DAMAGE = 9;
+    private static final int ADDITIONAL_DAMAGE = 3;
+
+    public FrenziedClaw() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+        setMagic(ADDITIONAL_DAMAGE, 2);
+        setDamage(DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+        tags.add(CustomTags.CLAW);
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int realBaseDamage = this.baseDamage;
+        this.baseDamage += this.magicNumber * countCards();
+        super.calculateCardDamage(mo);
+        this.baseDamage = realBaseDamage;
+        this.isDamageModified = (this.damage != this.baseDamage);
+    }
+
+    @Override
+    public void applyPowers() {
+        int realBaseDamage = this.baseDamage;
+        this.baseDamage += this.magicNumber * countCards();
+        super.applyPowers();
+        this.baseDamage = realBaseDamage;
+        this.isDamageModified = (this.damage != this.baseDamage);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (m != null)
+            addToBot(new VFXAction(new ClawEffect(m.hb.cX, m.hb.cY, Color.CYAN, Color.WHITE), 0.1F));
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+    }
+
+    public static int countCards() {
+        int count = 0;
+        for (AbstractCard c : AbstractDungeon.player.hand.group) {
+            if (isClaw(c))
+                count++;
+        }
+        for (AbstractCard c : AbstractDungeon.player.drawPile.group) {
+            if (isClaw(c))
+                count++;
+        }
+        for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
+            if (isClaw(c))
+                count++;
+        }
+        return count;
+    }
+
+    public static boolean isClaw(AbstractCard c) {
+        return c.hasTag(CustomTags.CLAW);
+    }
+}
