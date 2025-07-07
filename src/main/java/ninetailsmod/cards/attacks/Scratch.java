37f363d3b1db029f44f22b02ab08f253399fd9d9
Zoey Ling <zl64@rice.edu>
2025-07-06 17:22:33 -0700

    add scratch

---
+++ src/main/java/ninetailsmod/cards/attacks/Scratch.java
@@ -0,0 +1,39 @@
+package ninetailsmod.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import ninetailsmod.cards.BaseCard;
+import ninetailsmod.characters.NineTailsCharacter;
+import ninetailsmod.util.CardStats;
+
+public class Scratch extends BaseCard {
+
+    public static final String ID = makeID(Scratch.class.getSimpleName()); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    private static final CardStats info = new CardStats(
+            NineTailsCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    private static final int TIMES = 3;
+    private static final int UPG_TIMES = 1;
+
+    public Scratch() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(4);
+        setMagic(TIMES, UPG_TIMES);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < this.magicNumber; i++) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        }
+    }
+}
